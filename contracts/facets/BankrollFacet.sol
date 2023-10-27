333f3da727fe251854346498e89fe5adfa8c6bab
john j <johnjjdev690@gmail.com>
2023-10-27 23:42:42 +0000

    git add

--- contracts/facets/BankrollFacet.sol
+++ contracts/facets/BankrollFacet.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 
 import {WithStorage} from "../libraries/LibStorage.sol";
-import {LibDiamond} from "../vendor/libraries/LibDiamond.sol";
-import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {LibDiamond} from "../contracts/LibDiamond.sol";
+import {SafeERC20, IERC20} from "../contracts/ERC20/utils/SafeERC20.sol";
+
 
 /**
  * @title BankrollFacet, Contract responsible for keeping the bankroll and distribute payouts
