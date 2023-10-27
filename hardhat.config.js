74c3f11828d90f1f8051ebdc6137a05b9ab0a650
john j <johnjjdev690@gmail.com>
2023-10-27 23:06:29 +0000

    git add

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,8 @@
 /* global ethers task */
 require('@nomiclabs/hardhat-waffle')
 require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
